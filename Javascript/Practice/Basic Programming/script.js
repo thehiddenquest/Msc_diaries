e74503008290679488009bdaef51048cff677adb
Ninja <codehub5565@gmail.com>
2024-05-29 07:55:14 +0530

    javascript and assignment 2 of AOS

---
+++ Javascript/Practice/Basic Programming/script.js
@@ -0,0 +1,58 @@
+/*
+//Print in the console
+console.log("Hello world!!");
+
+//Print in evaluate 
+console.log(5+6*3);
+
+
+// Data types -- primitive{number,string, undefined , null, bigint , symbol}
+
+//number 
+let positiveNumber = 7;
+let negativeNumber = -9
+let decimalNumber = 12e5;
+let negativeDecimalNumber = 12e-5
+console.log(positiveNumber+" "+typeof(positiveNumber))
+console.log(negativeNumber+" "+typeof(negativeNumber))
+console.log(decimalNumber+" "+typeof(decimalNumber))
+console.log(negativeDecimalNumber+" "+typeof(negativeDecimalNumber))
+
+//string
+//It can be inserted by "" or '' or `` but `` supports "" and '' inside it and new line
+let firstName = "Rahul";
+let lastName = 'Biswas'
+
+let userInformation = ` Hi my name is "${firstName}" "${lastName}" `; //this is string laterals 
+console.log(userInformation)
+
+
+//String to number 
+let positiveNewNumber = "7"
+console.log(typeof(positiveNewNumber))
+
+let convertedNumber = +positiveNewNumber
+console.log(typeof(convertedNumber))
+
+let convertedNewString = +firstName
+console.log(convertedNewString + " " +typeof(convertedNewString))
+
+let conversionNumber = "1002303randomThing";
+let convertedNumberIs = parseInt(conversionNumber)
+console.log(`${convertedNumberIs} is type of ${typeof(convertedNumberIS)}`)
+
+//NaN is Not a Number
+
+// Boolean
+let truthValues = true
+let falseValue = false
+
+console.log(`${truthValues} is a type of ${typeof(truthValues)}`)
+
+//Undefined
+
+let newVariable
+let newUndefined = undefined
+
+console.log(` Both newVariable and newUndefined are the type of ${typeof(newUndefined)}`)
+*/
